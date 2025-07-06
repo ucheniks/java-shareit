9c929760fe415081e4834e0e59c594a350d086c5
Георгий Шельгаас <shelgaas00@mail.ru>
2025-07-06 04:26:59 +0300

    Добавить несколько тестов №2

--- server/src/main/java/ru/practicum/shareit/item/CommentRequestDTO.java
+++ server/src/main/java/ru/practicum/shareit/item/CommentRequestDTO.java
@@ -1,9 +1,6 @@
 package ru.practicum.shareit.item;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
